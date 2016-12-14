084996e483bf35991d0c1e02f2975b79a46b07bc
Rosenblatt <thomas.rosenblatt@netatmo.com>
2016-12-14 16:41:17 +0100

    Update NAScopes.php
    Add presence scopes

--- src/Netatmo/Common/NAScopes.php
+++ src/Netatmo/Common/NAScopes.php
@@ -11,10 +11,13 @@ class NAScopes
     const SCOPE_ACCESS_CAMERA = "access_camera";
     const SCOPE_READ_JUNE = "read_june";
     const SCOPE_WRITE_JUNE = "write_june";
+    const SCOPE_READ_PRESENCE = "read_presence";
+    const SCOPE_WRITE_PRESENCE = "write_presence";
+    const SCOPE_ACCESS_PRESENCE = "access_presence";
     static $defaultScopes = array(NAScopes::SCOPE_READ_STATION);
-    static $validScopes = array(NAScopes::SCOPE_READ_STATION, NAScopes::SCOPE_READ_THERM, NAScopes::SCOPE_WRITE_THERM, NAScopes::SCOPE_READ_CAMERA, NAScopes::SCOPE_WRITE_CAMERA, NAScopes::SCOPE_ACCESS_CAMERA, NAScopes::SCOPE_READ_JUNE, NAScopes::SCOPE_WRITE_JUNE);
+    static $validScopes = array(NAScopes::SCOPE_READ_STATION, NAScopes::SCOPE_READ_THERM, NAScopes::SCOPE_WRITE_THERM, NAScopes::SCOPE_READ_CAMERA, NAScopes::SCOPE_WRITE_CAMERA, NAScopes::SCOPE_ACCESS_CAMERA, NAScopes::SCOPE_READ_PRESENCE, NAScopes::SCOPE_WRITE_PRESENCE, NAScopes::SCOPE_ACCESS_PRESENCE, NAScopes::SCOPE_READ_JUNE, NAScopes::SCOPE_WRITE_JUNE);
     // scope allowed to everyone (no need to be approved)
-    static $basicScopes = array(NAScopes::SCOPE_READ_STATION, NAScopes::SCOPE_READ_THERM, NASCopes::SCOPE_WRITE_THERM, NAScopes::SCOPE_READ_CAMERA, NAScopes::SCOPE_WRITE_CAMERA, NAScopes::SCOPE_READ_JUNE, NAScopes::SCOPE_WRITE_JUNE);
+    static $basicScopes = array(NAScopes::SCOPE_READ_STATION, NAScopes::SCOPE_READ_THERM, NASCopes::SCOPE_WRITE_THERM, NAScopes::SCOPE_READ_CAMERA, NAScopes::SCOPE_WRITE_CAMERA, NAScopes::SCOPE_READ_PRESENCE, NAScopes::SCOPE_WRITE_PRESENCE, NAScopes::SCOPE_READ_JUNE, NAScopes::SCOPE_WRITE_JUNE);
 }
 
 ?>
